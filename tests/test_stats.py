78c7dbc74116fa4658600e558829a33dd71e55f4
Bruce Cutler <bruce@sumebrius.net>
2024-07-09 22:12:14 +1200

    Stats test

---
+++ tests/test_stats.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+
+from ffmpeg_asyncio.stats import Statistics
+
+
+def test_stats_line_parsing():
+    sample_lines = [
+        # ffmpeg version 6
+        "frame=   10 fps=7.0 q=-1.0 Lsize=    8733kB time=00:00:03.00 bitrate=23832.3kbits/s speed= 106x",
+        # ffmpeg version 7
+        "frame=   10 fps=7.0 q=-1.0 Lsize=    8733KiB time=00:00:03.00 bitrate=23832.3kbits/s speed= 106x",
+    ]
+
+    sample_lines_no_frame = [
+        # ffmpeg version 6
+        "size=    8733kB time=00:00:03.00 bitrate=23832.3kbits/s speed= 106x",
+        # ffmpeg version 7
+        "size=    8733KiB time=00:00:03.00 bitrate=23832.3kbits/s speed= 106x",
+    ]
+
+    for line in sample_lines:
+        assert Statistics.from_line(line) == Statistics(
+            frame=10,
+            fps=7.0,
+            size=8_942_592,
+            time=timedelta(seconds=3),
+            bitrate=23832.3,
+            speed=106,
+        )
+
+    for line in sample_lines_no_frame:
+        assert Statistics.from_line(line) == Statistics(
+            frame=0,
+            fps=0.0,
+            size=8_942_592,
+            time=timedelta(seconds=3),
+            bitrate=23832.3,
+            speed=106,
+        )
